f7dec669227a20a79bb64f35e967f28064d66390
GuinhoSensei <thiggouveia@hotmail.com>
2024-11-09 16:37:19 -0800

    sales total

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
-﻿using Newtonsoft.Json; 
+﻿using Newtonsoft.Json;
+using System.Text;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir);   
+Directory.CreateDirectory(salesTotalDir);
 
 var salesFiles = FindFiles(storesDirectory);
 
-var salesTotal = CalculateSalesTotal(salesFiles);
+var salesTotal = CalculateSalesTotal(salesFiles, out StringBuilder salesDetails);
 
 File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+// Create the sales summary report file
+var salesSummaryFilePath = Path.Combine(salesTotalDir, "SalesSummaryReport.txt");
+File.WriteAllText(salesSummaryFilePath, GenerateSalesSummary(salesTotal, salesDetails));
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -30,24 +35,42 @@ IEnumerable<string> FindFiles(string folderName)
     return salesFiles;
 }
 
-double CalculateSalesTotal(IEnumerable<string> salesFiles)
+double CalculateSalesTotal(IEnumerable<string> salesFiles, out StringBuilder salesDetails)
 {
     double salesTotal = 0;
-    
+    salesDetails = new StringBuilder();
+
     // Loop over each file path in salesFiles
     foreach (var file in salesFiles)
-    {      
+    {
         // Read the contents of the file
         string salesJson = File.ReadAllText(file);
-    
+
         // Parse the contents as JSON
         SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
-    
+
         // Add the amount found in the Total field to the salesTotal variable
-        salesTotal += data?.Total ?? 0;
+        double fileTotal = data?.Total ?? 0;
+        salesTotal += fileTotal;
+
+        // Append details for the current file
+        salesDetails.AppendLine($" {Path.GetFileName(file)}: {fileTotal.ToString("C")}");
     }
-    
+
     return salesTotal;
 }
 
-record SalesData (double Total);
+string GenerateSalesSummary(double totalSales, StringBuilder details)
+{
+    StringBuilder reportBuilder = new StringBuilder();
+    reportBuilder.AppendLine("Sales Summary");
+    reportBuilder.AppendLine("----------------------------");
+    reportBuilder.AppendLine($" Total Sales: {totalSales.ToString("C")}");
+    reportBuilder.AppendLine();
+    reportBuilder.AppendLine(" Details:");
+    reportBuilder.Append(details.ToString());
+
+    return reportBuilder.ToString();
+}
+
+record SalesData(double Total);
